fd8e2fc1ba6d2ee9ccee1de6e971ecaf2995962e
Ankit Raj <ankit.1998.ar@gmail.com>
2019-07-18 22:59:58 +0530

    updated data preprocessing

--- Maching learning basics/Data preprocessing template/data_preprocessing_template.py
+++ Maching learning basics/Data preprocessing template/data_preprocessing_template.py
@@ -35,3 +35,17 @@ X = onehotencoder.fit_transform(X).toarray()
 
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
+
+#Splitting the dataset into train and test set
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2,random_state=0)
+
+#feature scaling (Since ML models uses Euclidian distance between the features, the scale may become large)
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+
