6a9420930d84d54632aabaf1251d82ed0247e899
ankit <ankit.ar@gmail.com>
2019-07-18 21:20:49 +0530

    data preprocessing

---
+++ Maching learning basics/Data preprocessing template/data_preprocessing_template.py
@@ -0,0 +1,10 @@
+# Pre processing of data
+
+# importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing the dataset
+
+data = pd.read_csv('Data.csv')
